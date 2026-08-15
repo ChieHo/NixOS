a8f1926dccccf7717a1d0fc0c087b9798190e85d
ChieHo <chieho.c@tutanota.com>
2026-08-15 22:08:37 +0200

    some packages

--- configuration.nix
+++ configuration.nix
@@ -101,9 +101,18 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  git
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
+    syncthing
+    keepassxc
+    thunderbird
+    kdePackages.oxygen
+    librewolf
+    discord
+    htop
+    libreoffice
+    mullvad
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
